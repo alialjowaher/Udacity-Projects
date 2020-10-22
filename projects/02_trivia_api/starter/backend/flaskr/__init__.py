235ee975a68b4f4d16a9514c26dd26f077bb1ba8
Ali AlJowaher <alinet010@gmail.com>
2020-10-22 15:02:10 +0300

    added endpoints

--- projects/02_trivia_api/starter/backend/flaskr/__init__.py
+++ projects/02_trivia_api/starter/backend/flaskr/__init__.py
@@ -1,5 +1,8 @@
 import os
-from flask import Flask, request, abort, jsonify
+from flask import Flask, request, abort, jsonify, redirect
+from flask.helpers import url_for
+from sqlalchemy.sql.base import NO_ARG
+from werkzeug import datastructures
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
@@ -8,48 +11,119 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-  # create and configure the app
-  app = Flask(__name__)
-  setup_db(app)
-  
+    # create and configure the app
+    app = Flask(__name__)
+    setup_db(app)
+    '''
+  @DONE: Set up CORS. Allow '*' for origins. Delete the sample route
+  after completing the TODOs
   '''
-  @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
+    CORS(app)
+   # cors = CORS(app, resources={r"*": {"origins": "*"}})
+    '''
+  @DONE: Use the after_request decorator to set Access-Control-Allow
   '''
-
-  '''
-  @TODO: Use the after_request decorator to set Access-Control-Allow
-  '''
-
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests 
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers',
+                             'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PUT,POST,DELETE,OPTIONS')
+        return response
+    '''
+  @DONE:
+  Create an endpoint to handle GET requests
   for all available categories.
   '''
+    @app.route('/categories', methods=['GET'])
+    def get_categories():
+        all_categories = Category.query.order_by(Category.id).all()
+        categories = []
 
+        # @TODO: return 404 formated error
+        if all_categories is None:
+            abort(404)
+        else:
 
-  '''
-  @TODO: 
-  Create an endpoint to handle GET requests for questions, 
-  including pagination (every 10 questions). 
-  This endpoint should return a list of questions, 
-  number of total questions, current category, categories. 
+            for category in all_categories:
+                categories.append(category.type)
+
+            return jsonify({
+                'success': True,
+                'categories': categories
+            })
+
+    '''
+  @DONE:
+  Create an endpoint to handle GET requests for questions,
+  including pagination (every 10 questions).
+  This endpoint should return a list of questions,
+  number of total questions, current category, categories.
 
   TEST: At this point, when you start the application
   you should see questions and categories generated,
   ten questions per page and pagination at the bottom of the screen for three pages.
-  Clicking on the page numbers should update the questions. 
+  Clicking on the page numbers should update the questions.
   '''
+    def pagination(request, selection):
+        page = request.args.get('page', 1, type=int)
+        start = (page - 1) * QUESTIONS_PER_PAGE
+        end = start + QUESTIONS_PER_PAGE
+        questions = [question.format() for question in selection]
+        current_questions = questions[start:end]
+        return current_questions
 
-  '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+    @app.route('/questions', methods=['GET'])
+    def get_questions():
+        all_questions = Question.query.order_by(Question.id).all()
+        current_questions = pagination(request, all_questions)
+
+        # @TODO: return 404 formated error
+        if len(current_questions) == 0:
+            abort(404)
+        else:
+            all_categories = Category.query.order_by(Category.id).all()
+            categories = []
+            for category in all_categories:
+                categories.append(category.type.lower())
+
+            return jsonify({
+                'success': True,
+                'questions': current_questions,
+                'total_questions': len(all_questions),
+                'categories': categories,
+
+            })
+
+    '''
+  @DONE:
+  Create an endpoint to DELETE question using a question ID.
 
   TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
+  This removal will persist in the database and when you refresh the page.
   '''
+    @app.route('/questions/<int:question_id>', methods=['DELETE'])
+    def delete_question(question_id):
 
-  '''
+        question = Question.query.get(question_id)
+
+        if question is None:
+            abort(404)
+
+        try:
+            question.delete()
+            # front-end handles redirect on success message
+            return jsonify({
+                'success': True,
+                'question_id': question_id,
+            })
+
+        except:
+            abort(422)
+
+        '''
   @TODO: 
   Create an endpoint to POST a new question, 
   which will require the question and answer text, 
@@ -59,8 +133,37 @@ def create_app(test_config=None):
   the form will clear and the question will appear at the end of the last page
   of the questions list in the "List" tab.  
   '''
+    @app.route('/questions', methods=['POST'])
+    def create_question():
+    
+                  content = request.get_json()
+                  question = content.get('question')
+                  answer = content.get('answer')
+                  difficulty = content.get('difficulty')
+                  category = content.get('category')
+                  
+                  
+                  if not question or not answer or not difficulty or not category:
+                      abort(422)  
+                  
+                  try:
+                      new_question = Question(question=question,answer=answer,
+                      difficulty=difficulty,category=category)
+                      new_question.insert()
+                      all_questions = Question.query.order_by(Question.id).all()
+                      current_questions = pagination(request, all_questions)
+                  
 
-  '''
+                      return jsonify({
+                        'success': True,
+                        'message':'The Question has been added Successfully !',
+                      })
+                  except Exception:
+                      abort(422)
+                  
+
+
+    '''
   @TODO: 
   Create a POST endpoint to get questions based on a search term. 
   It should return any questions for whom the search term 
@@ -70,8 +173,24 @@ def create_app(test_config=None):
   only question that include that string within their question. 
   Try using the word "title" to start. 
   '''
-
-  '''
+    @app.route('/questions/search', methods=['POST'])
+    def search_question():
+        search = request.get_json()
+        search_term = search.get('searchTerm','')
+        print(search)
+        print(search_term)
+        try:
+            search_questions = Question.query.filter(Question.question.ilike(f'%{search_term}%')).all()
+            current_questions = pagination(request, search_questions)
+            return jsonify({
+              'success': True,
+              'questions': current_questions,
+              'total_questions': len(search_questions)
+            })
+        except:
+          abort(404)
+    
+    '''
   @TODO: 
   Create a GET endpoint to get questions based on category. 
 
@@ -80,8 +199,7 @@ def create_app(test_config=None):
   category to be shown. 
   '''
 
-
-  '''
+    '''
   @TODO: 
   Create a POST endpoint to get questions to play the quiz. 
   This endpoint should take category and previous question parameters 
@@ -93,12 +211,10 @@ def create_app(test_config=None):
   and shown whether they were correct or not. 
   '''
 
-  '''
+    '''
   @TODO: 
   Create error handlers for all expected errors 
   including 404 and 422. 
   '''
-  
-  return app
 
-    
+    return app
